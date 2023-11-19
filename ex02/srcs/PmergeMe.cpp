fc58a5fd1123e6aecef7b075f3e4e5665224f9b7
Lucas <lucash1lucashumbert166@gmail.com>
2023-11-20 00:23:32 +0100

    clean

--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -6,12 +6,13 @@
 /*   By: humbert <humbert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 15:26:12 by humbert           #+#    #+#             */
-/*   Updated: 2023/11/15 21:29:44 by humbert          ###   ########.fr       */
+/*   Updated: 2023/11/20 00:19:51 by humbert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+//PRINT 5 ELEMENTS DU VECTEUR
 void    print_vector(std::vector<int> vec) {
     std::vector<int>::iterator  it;
     int                         i = 0;
@@ -26,6 +27,7 @@ void    print_vector(std::vector<int> vec) {
     std::cout << std::endl;
 }
 
+//TRIE VECTEUR PAR INSERTION
 std::vector<int>    insertionSort_vector(std::vector<int> vec) {
     int key;
     int j;
@@ -41,6 +43,7 @@ std::vector<int>    insertionSort_vector(std::vector<int> vec) {
     return (vec);
 }
 
+//TRIE DEQEU PAR INSERTION
 std::deque<int>     insertionSort_deque(std::deque<int> deq) {
     int key;
     int j;
@@ -56,6 +59,7 @@ std::deque<int>     insertionSort_deque(std::deque<int> deq) {
     return (deq);
 }
 
+//PRINT 5 ELEMENTS DU DEQUE
 void    print_deque(std::deque<int> deq) {
     std::deque<int>::iterator   it;
     int                         i = 0;
@@ -70,6 +74,7 @@ void    print_deque(std::deque<int> deq) {
     std::cout << std::endl;
 }
 
+//ALGO
 bool    PmergeMe(char **argv) {
     std::vector<int>    vec;
     std::vector<int>    sorted_vec;
@@ -80,6 +85,7 @@ bool    PmergeMe(char **argv) {
     clock_t             start_vec, end_vec;
     clock_t             start_deq, end_deq;
 
+    //Parcours args et verifie int
     for (int j = 1; argv[j]; j++) {
         for (int i = 0; argv[j][i]; i++) {
             if (!std::isdigit(argv[j][i])) {
@@ -88,16 +94,20 @@ bool    PmergeMe(char **argv) {
                 return (std::cout << "Error: non digit character" << std::endl, true);
             }
         }
+        //Conversion char to int
         number = std::strtol(argv[j], NULL, 10);
         str = argv[j];
         if (number > 2147483647 || str.size() > 10)
             return (std::cout << "Error: overflow" << std::endl, false);
+        //Ajout au vec et deque
         vec.push_back(number);
         deq.push_back(number);
     }
+    //Affiche contenu initial vec
     std::cout << "Before: ";
     print_vector(vec);
-    
+
+    //Mesure temps de tri vec 
     start_vec = clock();
     sorted_vec = insertionSort_vector(vec);
     end_vec = clock();
@@ -105,11 +115,13 @@ bool    PmergeMe(char **argv) {
     std::cout << "After: ";
     print_vector(sorted_vec);
 
+    //Mesure temps de tri deque
     start_deq = clock();
     sorted_deq = insertionSort_deque(deq);
     end_deq = clock();
     long micro_deq = ((double)end_deq - (double)start_deq) / CLOCKS_PER_SEC * 1000000;
 
+    //Affiche temps traitement vec et deque
     std::cout << "Time to process a range of " << vec.size() \
     << " elements with std::vector : " << micro_vec << "µs" << std::endl;
     std::cout << "Time to process a range of " << deq.size() \
